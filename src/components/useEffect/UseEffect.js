d5aae9124861f0d5084bd98ccdca0bb5332f67e6
Wilson You <wayneyou@126.com>
2021-04-04 20:51:00 -0600

    updated

---
+++ src/components/useEffect/UseEffect.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+
+function UseEffect() {
+    const [resourceType, setResourceType] = useState('posts')
+    const [items, setItems] = useState([])
+    const [windowWith, setWindowWith] = useState(window.innerWidth)
+
+    const handleResize = () => {
+        setWindowWith(window.innerWidth)
+    }
+    useEffect(() => {
+
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+            .then(response => response.json())
+            .then(json => setItems(json))
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [resourceType])
+    return (
+        <>
+            <div className='useeffect-container'>
+                <div className='btn-container'>
+                    <button onClick={() => setResourceType('posts')}>Posts</button>
+                    <button onClick={() => setResourceType('users')}>Users</button>
+                    <button onClick={() => setResourceType('comments')}>Comments</button>
+                </div>
+                <div><p className='resource'>{resourceType}</p></div>
+            </div>
+
+            <div>
+                <p className='showWidth'>{windowWith}</p>
+            </div>
+            <div className='items'>
+                {items.map(item => {
+                    return <pre>{JSON.stringify(item)}</pre>
+                })}
+            </div>
+
+        </>
+    )
+}
+
+export default UseEffect
